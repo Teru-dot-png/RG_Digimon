2e3fb993c72e2d83ce5b1c11783bf4aef2cc48ba
Teru.png <68662335+Teru-dot-png@users.noreply.github.com>
2022-12-15 05:47:45 +0000

    time system and future sleep and lights off system

--- CPU0.lua
+++ CPU0.lua
@@ -11,7 +11,7 @@ local but1 = gdt.LedButton1
 local but2 = gdt.LedButton2
 
 -- advance one frame
-local frameDuration = 1.5
+local frameDuration = 1
 
 -- this will keep track of DeltaTime
 local deltaCounter = 0
@@ -19,10 +19,23 @@ local deltaCounter = 0
 -- keep track of the current frame
 local frameNumber = 0 
 
+-- Time data
+local seconds = 0
+local minutes = 0
+local hours = 0
+local days = 0
+local weeks = 0
+local months = 0
+local years = 0
+
+
+
+
+
 -- keep track of menu 
 local menuPos = 0
 local maxMenuPos = 9
-local isInsideMenu
+local isInsideMenu = false
 
 -- keep track of position of selector
 local selectorPos:vec2 = vec2(0,5)
@@ -34,6 +47,10 @@ local DigimonPos:vec2 = vec2(0,0)
 local digimonposX = 35
 local digimonposY = 25
 local looking = 0
+local sleeping = false
+
+-- keep track of lights
+local lightsOff = false
 
 -- as much as i dont want to we need to keep track of poop
 local poopR = 0
@@ -50,6 +67,52 @@ local flushCount = 0
 local poopFlushQueue = 0
 local flushPosX = 55
 local flushPosY = 16
+
+function timeTracker()
+  -- seconds
+  seconds += 1
+  if seconds == 60 then
+    seconds = 0
+    -- minutes
+    minutes += 1
+    if minutes == 60 then
+      minutes = 0
+      -- hours
+      hours += 1
+      if hours == 24 then
+        hours = 0
+        -- days
+        days += 1
+        if days == 7 then
+          days = 0
+          -- weeks
+          weeks += 1
+          if weeks == 4 then
+            weeks = 0
+            -- month
+            months += 1
+            if months == 12 then
+              month = 0
+              -- years
+              years += 1
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+function deltaTimeHandler()
+  while (deltaCounter >= frameDuration) do
+    deltaCounter -= frameDuration
+    frameNumber += 1
+    timeTracker()
+    poopAnim = math.random(2, 3)
+    digimonMover()
+    flushPoop()
+  end
+end
 
 -- this will draw menu sprites
 function drawMenuSprites()
@@ -236,81 +299,74 @@ end
 function debugPrint()
 print(
 "  Digimon X Y Pos:" .. digimonposX .. " " .. digimonposY .. "\n",
+"Flush X Y Pos: " .. flushPosX .. " " .. flushPosY .. "\n",
 "Current Menu:" .. menuPos .. "\n",
-"poop data:" .. poopValue .. "\n",
-"Flush X Y Pos: " .. flushPosX .. " " .. flushPosY
+"poopV:" .. poopValue .. "\n",
+"T: " .. seconds .. " " .. minutes .. " " .. hours .. "\n",
+"dlt-T: " .. deltaCounter
 )
 end
 
 
 -- ######## MAIN GAME LOOP ########
 function update()
-    debugPrint()
-
-    -- clears the screen
-    vid:Clear(color.white)
-
-    -- set according possitions
-    selectorPos = vec2(selposX,selposY)
-    DigimonPos = vec2(digimonposX, digimonposY)
-    poopPos = vec2(PoopPosX, PoopPosY)
+  debugPrint()
+  
+  -- clears the screen
+  vid:Clear(color.white)
+  
+  -- set according possitions
+  selectorPos = vec2(selposX,selposY)
+  DigimonPos = vec2(digimonposX, digimonposY)
+  poopPos = vec2(PoopPosX, PoopPosY)
+  
+  
+  -- increase the counter by the CPU's DeltaTime
+  deltaCounter += gdt.CPU0.DeltaTime
+  
+  -- this function will draw the menu sprites
+  drawMenuSprites()
+  
+  -- draws the cursor
+  drawSelSprite()
+  
+  -- does colision for digimon
+  digimonColision()
+  
+  -- draws the digimon
+  drawDigimon()
+  
+  -- handdler for digimon stuff
+  digimonHandler()
+  
+  -- draw funny poopoo
+  drawPoop()
+  
+  -- draws the little waves to flush shit
+  drawflush()
+  
+  
+  -- checks if the button is pressed down to cycle tru menu
+  if but2.ButtonDown then
+    CursorHandler()
     
-
-    -- increase the counter by the CPU's DeltaTime
-    deltaCounter += gdt.CPU0.DeltaTime
-    
-    -- this function will draw the menu sprites
-    drawMenuSprites()
-    
-    -- draws the cursor
-    drawSelSprite()
-    
-    -- does colision for digimon
-    digimonColision()
-    
-    -- draws the digimon
-    drawDigimon()
-    
-    -- handdler for digimon stuff
-    digimonHandler()
-    
-    -- draw funny poopoo
-    drawPoop()
-
-    -- draws the little waves to flush shit
-    drawflush()
-
-    
-    -- checks if the button is pressed down to cycle tru menu
-    if but2.ButtonDown then
-        CursorHandler()
-				
-    end
-    
-    if but1.ButtonDown then
-        menuSelect()
-    end
-
-    if but0.ButtonDown then
-        poop = true
-        poopValue += 1
-    end
-
-    
-    
-    
-    
-    -- we run a while loop for however many times the delta counter is greater
-    -- than the duration of one frame, such that if for some reason the counter
-    -- has gone past over one frame duration, we run our frame loop however many
-    -- times necessary to keep the logic tied to the timer.
-    while (deltaCounter >= frameDuration) do
-        deltaCounter -= frameDuration
-        frameNumber += 1
-        poopAnim = math.random(2, 3)
-        digimonMover()
-        flushPoop()
-							
-		end
-	  
+  end
+  
+  if but1.ButtonDown then
+    menuSelect()
+  end
+  
+  if but0.ButtonDown then
+    poop = true
+    poopValue += 1
+  end
+  
+  
+  
+  
+  -- time tracker 
+  deltaTimeHandler()
+  
+  
+  
 end
