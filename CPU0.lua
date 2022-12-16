79bc50a95a173e30dad3bc11a78c4d0327bbd92a
Teru-dot-png <luiz.renato@tuta.io>
2022-12-16 18:04:45 -0300

    Sleep checks and optimizations

--- CPU0.lua
+++ CPU0.lua
@@ -10,6 +10,11 @@ local but0 = gdt.LedButton0
 local but1 = gdt.LedButton1
 local but2 = gdt.LedButton2
 
+-- keep track of room info
+local room = {
+lights = true,
+r = 0
+}
 
 -- advance one frame
 -- this will keep track of DeltaTime
@@ -34,11 +39,12 @@ local time = {
 
 
 -- flush data 
-local flushing = false
-local flushCount = 0
-local poopFlushQueue = 0
-local flushPosX = 55
-local flushPosY = 16
+flush = {
+ing = false,
+queue = 0,
+posX = 55,
+posY = 16
+}
 
 -- keep track of menu
 local menu = { 
@@ -71,10 +77,18 @@ local menu = {
     end},
     {name = "flush", action = function() 
       print("Menu position 4 selected") 
-      poopFlushQueue = 1 
+      if room.lights == true then
+      flush.queue = 1 
+      flush.ing = 1
+      end
     end},
     {name = "lights", action = function() 
-      print("Menu position 5 selected") 
+      print("Menu position 5 selected")
+      if room.lights == false then
+      room.lights = true 
+      elseif room.lights == true then
+      room.lights = false
+      end
     end},
     {name = "patch", action = function() 
       print("Menu position 6 selected") 
@@ -109,26 +123,25 @@ local cursor = {
 }
 
 -- keep track of digimon position and stats
-local DigimonPos:vec2 = vec2(0,0)
 local digimon = {
+   pos = vec2(0,0),
    posX = 35,
    posY = 25,
+   sleepTime0 = 0,
    looking = 0,
    sleeping = false
 }
 
--- keep track of room info
-local lights = false
-
 -- as much as i dont want to we need to keep track of poop
-local poopR = 0
-local poopValue = 0
-local poop = false
-local poopAnim = 0
-local poopPos = vec2(0, 0)
-local PoopPosX = 35
-local PoopPosY = 35
-
+ poop = {
+ r = 0,
+ value = 0,
+ hasHappend = false,
+ anim = 0,
+ pos = vec2(0, 0),
+ posX = 35,
+ posY = 35
+}
 
 function timeTracker()
   -- seconds
@@ -169,9 +182,13 @@ function deltaTimeHandler()
   while (timeDlt.counter >= timeDlt.frameDuration) do
     timeDlt.counter -= timeDlt.frameDuration
     timeDlt.frameNum += 1
-    poopValue += 1
+    poop.value += 1
     timeTracker()
-    poopAnim = math.random(2, 3)
+    poop.anim = math.random(2, 3)
+    room.r = math.random(0, 1)
+
+    
+
     digimonMover()
     flushPoop()
   end
@@ -180,8 +197,24 @@ end
 -- this will draw menu sprites
 function drawMenuSprites()
 
+  if flush.ing == false then
+   if room.lights == false then
+     vid:FillRect(vec2(5, 15), vec2(72, 48), color.black)
+
+    if digimon.sleeping == true then
+     vid:DrawSprite(vec2(digimon.posX + room.r ,digimon.posY + room.r), menuSprites, 7, room.r, color.white, color.white)
+  
+    elseif digimon.sleeping == false then
+    vid:DrawSprite(vec2(digimon.posX + room.r ,digimon.posY + room.r), menuSprites, 7, 2, color.white, color.white)
+    end
+   end
+  end
+
   -- makes the menu borders
-  vid.DrawRect(vid, vec2(5, 15), vec2(72, 48), color.black)
+  vid:DrawRect(vec2(5, 15), vec2(72, 48), color.black)
+  vid:FillRect(vec2(0, 15), vec2(4, 48), color.white)
+  vid:FillRect(vec2(73, 15), vec2(79, 48), color.white)
+
 
 
   -- Draws the top menu
@@ -227,6 +260,7 @@ end
 -- this function will move the digimon once and a while
 function digimonMover()
     
+  if digimon.sleeping == false then
     -- random number for looking left or right
     digimon.looking = math.random()
     
@@ -239,15 +273,34 @@ function digimonMover()
         
         digimon.posX -= 5
     end
-    
+  end
     
 end
 
 -- this function will handdle the digimon stats and needs
 function digimonHandler()
-    
+  -- digimon time counter
+  digimon.sleepTime0 += gdt.CPU0.DeltaTime
 
+  -- Clamp the value of sleepTime0 to a range of 0 to 28800 (8 hours)
+  digimon.sleepTime0 = math.clamp(digimon.sleepTime0, 0, 28800)
+
+  -- Check if the digimon is currently sleeping
+  if digimon.sleeping then
+    -- If the digimon has been sleeping for at least 2 hours, set sleeping to false
+    if digimon.sleepTime0 >= 7200 then
+      digimon.sleepTime0 -= 7200
+      digimon.sleeping = false
+    end
+  else
+    -- If the digimon has been awake for at least 8 hours, set sleeping to true
+    if digimon.sleepTime0 >= 28800 then
+      digimon.sleepTime0 -= 28800
+      digimon.sleeping = true
+    end
+  end
 end
+
 
 
 --this is like a vibe check but to see if you shat yourself
@@ -255,39 +308,40 @@ function poopCheck()
 
 
 
-    poopR = math.random( 1, 3150 )
+    poop.r = math.random( 1, 13150 )
 
-    
   
-
-    if poopR == 500 or poopR == 200 or poopR == 1000 or poopValue >= 10800 then
-    poop = true
-    poopValue = 0
+    if poop.r == 500 or poop.value >= 10800 then
+    poop.hasHappend = true
+    poop.value = 0
     end
 end
 
 -- this function will draw poop if digimon has done the peepee poopoo caacaa
 function drawPoop()
-    if poop == true then
-    vid:DrawSprite(poopPos, menuSprites, 6, poopAnim, color.white, color.clear)
+    if poop.hasHappend == true then
+    vid:DrawSprite(poop.pos, menuSprites, 6, poop.anim, color.white, color.clear)
     end
 end
 
 function flushPoop()
 
   -- check if we made a flush request
-  if poopFlushQueue > 0.5 then
+  if flush.queue > 0.5 then
    
     -- move the water left
-      flushPosX += -10
+      flush.posX += -10
     
 
     -- if flush moved out of screen we set all values to default
-    if flushPosX < 2 then
-    poop = false
-    poopValue = 0
-    poopFlushQueue = 0
-    flushPosX = 55
+    if flush.posX < 2 then
+    flush.queue = 0
+    flush.posX = 55
+    flush.ing = false
+      if poop.hasHappend == true then
+      poop.hasHappend = false
+      poop.value = 0
+      end
     end
   end
 end
@@ -295,9 +349,9 @@ end
 -- draws the thing that flushes the shiz
 function drawflush()
 
-  if poopFlushQueue > 0.5 then
-  vid:DrawSprite(vec2(flushPosX, flushPosY), menuSprites, 4, 3, color.white, color.clear)
-  vid:DrawSprite(vec2(flushPosX, flushPosY + 16), menuSprites, 4, 3, color.white, color.clear)
+  if flush.queue > 0.5 then
+  vid:DrawSprite(vec2(flush.posX, flush.posY), menuSprites, 4, 3, color.white, color.clear)
+  vid:DrawSprite(vec2(flush.posX, flush.posY + 16), menuSprites, 4, 3, color.white, color.clear)
   end
 
 end
@@ -305,13 +359,20 @@ end
 -- this function will handdle the digimon sprites
 function drawDigimon()
 
-
+  if digimon.sleeping == false then
     -- if its looking left or right we use diferent sprites 
     if digimon.looking < 0.5 then 
-        vid:DrawSprite(DigimonPos, digimonSpritesFlip, 0, 1, color.white, color.clear)
+        vid:DrawSprite(digimon.pos, digimonSpritesFlip, 0, 1, color.white, color.clear)
     else
-        vid:DrawSprite(DigimonPos, digimonSprites, 0, 1, color.white, color.clear)
+        vid:DrawSprite(digimon.pos, digimonSprites, 0, 1, color.white, color.clear)
     end
+  else
+    if digimon.looking < 0.5 then 
+    vid:DrawSprite(digimon.pos, digimonSprites, 1, 3, color.white, color.clear)
+    else
+    vid:DrawSprite(digimon.pos, digimonSpritesFlip, 1, 3, color.white, color.clear)
+    end
+  end
 end
 
 --  we do some colision checking in the rectangle
@@ -327,18 +388,25 @@ end
 
 -- prints the debug info
 function debugPrint()
+    
   print(
-  "  Digimon X Y Pos:" .. digimon.posX .. " " .. digimon.posY .. "\n",
-  "Flush X Y Pos: " .. flushPosX .. " " .. flushPosY .. "\n",
-  "MenuItem:" .. menu.current .. "\n",
-  "ShitData:" .. poopValue .. poopFlushQueue .. "\n",
-  "T: " .. time.seconds .. " " .. time.minutes .. " " .. time.hours .. "\n",
-  "dlt-T: " .. timeDlt.counter
+    "  MenuItem: " .. menu.current .. "\n",
+    "room " .. "l" .. tostring(room.lights) .. " " .. "F" .. tostring(flush.ing) .. "\n",
+    "DigimonInfo " .. digimon.sleepTime0 .. " " .. tostring(digimon.sleeping) .. "\n",
+  
+  --"Flush X Y Pos: " .. flush.posX .. " " .. flush.posY .. "\n",
+  --"ShitData:" .. poop.value .. " " .. flush.queue .. "\n",
+  --"dlt-T: " .. timeDlt.counter .. "\n",
+  --"CPU-D: " .. gdt.CPU0.DeltaTime .. "\n",
+  "S" .. time.seconds .. " M" .. time.minutes .. " H" .. time.hours
   )
 end
 
 
+
+-- ################################
 -- ######## MAIN GAME LOOP ########
+-- ################################
 function update()
   debugPrint()
   
@@ -347,50 +415,58 @@ function update()
   
   -- set according possitions
   cursor.pos = vec2(cursor.posX,cursor.posY)
-  DigimonPos = vec2(digimon.posX, digimon.posY)
-  poopPos = vec2(PoopPosX, PoopPosY)
+  digimon.pos = vec2(digimon.posX, digimon.posY)
+  poop.pos = vec2(poop.posX, poop.posY)
   
   
   -- increase the counter by the CPU's DeltaTime
   timeDlt.counter += gdt.CPU0.DeltaTime
 
-  -- this function will draw the menu sprites
-  drawMenuSprites()
-  
   -- draws the cursor
   drawSelSprite()
   
   -- does colision for digimon
   digimonColision()
-  
-  -- draws the digimon
-  drawDigimon()
-  
+
   -- handdler for digimon stuff
   digimonHandler()
   
-  -- draw funny poopoo
-  drawPoop()
-
   -- checks if you shat yourself
   poopCheck()
+  
+  -- draw funny poopoo
+  drawPoop()
   
   -- draws the little waves to flush shit
   drawflush()
   
+  -- draws the digimon
+  drawDigimon()
   
-  -- checks if the button is pressed down to cycle tru menu
+  
+  -- this function will draw the menu sprites
+  drawMenuSprites()
+  
+  
+
   if but2.ButtonDown then
+    if flush.ing == false and room.lights == true then
+    -- every time we exec this cursor moves to apropiate place
+    -- cycle tru menu
     CursorHandler()
-    
+  end
   end
   
   if but1.ButtonDown then
+    -- selects the item you where hovered over
     menu:select()
   end
   
   if but0.ButtonDown then
-    poopValue += 1000
+    -- debug shityourself button
+    -- poop.value += 4
+    -- digimon.posX += 4
+    digimon.sleepTime0 += 28799
   end
   
   
@@ -398,7 +474,4 @@ function update()
   
   -- time tracker 
   deltaTimeHandler()
-  
-  
-  
 end
