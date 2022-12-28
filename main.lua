6c9adc8a0d1cc63557fb2b816a4cc97c4e45927a
Teru-dot-png <luiz.renato@tuta.io>
2022-12-28 08:35:06 -0300

    a faliure at trying to do a anim  got to keep goin

--- main.lua
+++ main.lua
@@ -1,17 +1,31 @@
 
 --! EventCH
-gdt.CPU0.EventChannels[1] = gdt.Wifi0
+gdt.CPU0.EventChannels[1] = gdt.Wifi0 -- Adding the Wifi chip as the first element of this array, so that it can trigger events for the CPU to handle.
+--[[
+ This line of code initializes a table named handleFuncs, which maps request handles (numbers) 
+to functions that accept a WifiWebResponseEvent parameter and return nothing 
+(i.e. (result: WifiWebResponseEvent) -> ()).
+This table is used to store functions that should be called when a web request's
+response event is received.
+
+]]--
 local handleFuncs: {[number]: (result: WifiWebResponseEvent) -> ()} = {}
 
 
 --! Assets 
-local menuSprites:SpriteSheet = gdt.ROM.User.SpriteSheets.Digimon1 -- menu Sprites for main game
-local bgs:SpriteSheet = gdt.ROM.User.SpriteSheets.bgs -- menu Sprites for main game
+local menuSprites:SpriteSheet = gdt.ROM.User.SpriteSheets.Digimon1 -- Menu Sprites for main game
+local bgs:SpriteSheet = gdt.ROM.User.SpriteSheets.bgs -- Background sprites for the game
 local digimonSprites:SpriteSheet = gdt.ROM.User.SpriteSheets.digimonNIGHTMARE1 -- digimon Sprites looking left
 local digimonSpritesFlip:SpriteSheet = gdt.ROM.User.SpriteSheets.digimonNIGHTMARE1Flip -- digimon Sprites looking right
 
+--! Code modules
+local dt = require("debugTools")
+local debugPrint = dt.debugPrint
+
+
 
 --! Hardware
+local cpu:CPU = gdt.CPU0
 local vid:VideoChip = gdt.VideoChip0 -- graphics chip
 local web:Wifi = gdt.Wifi0 -- wifi web conectivity
 local but0 = gdt.LedButton0 -- bottom button
@@ -21,26 +35,30 @@ local but2 = gdt.LedButton2 -- top button
 --!----------------------------------------------------------------------------
 --!----   EventCH 1      ------------------------------------------------------
 --!----------------------------------------------------------------------------
-
-
-
+--[[
+ This function is an event handler for the Wifi module, specifically for the WifiWebResponseEvent.
+When this event is triggered, it is passed to this function along with the Wifi module that triggered it.
+The function then looks up a function in the handleFuncs table using the RequestHandle from the event as the key.
+This function is then called with the event as its argument. Finally, the function stored in handleFuncs
+for the given RequestHandle is removed. This allows for the ability to associate a specific function with 
+a specific web request, allowing for better organization and management of web requests in the code.
+]]--
 function eventChannel1(_: Wifi, event: WifiWebResponseEvent)
   handleFuncs[event.RequestHandle](event)
   handleFuncs[event.RequestHandle] = nil
 end
-
-
 --!----------------------------------------------------------------------------
 
 
 --? Flag to enable debugging messages
-local debugB = true
+local debugBool = true
 
 --? keep track of room info
 local room = {
 lights = true, -- room lights if on then true if off then false
 r = 0 -- random number  between (1, 0)
 }
+
 
 
 --? keep track of time deltas and frames
@@ -59,9 +77,11 @@ local time = {
   weeks = 0,
   months = 0,
   years = 0,
-  condition = false,
-  counter = 0
-}
+  counter = 0,
+  health = {
+  condition = false, -- a warning to see if the condition of the time is bad and unupdated
+  updating = true -- if its updating or not 
+}}
 
   --? keep track of digimon position and stats
   local digimon = {
@@ -69,7 +89,7 @@ local time = {
     r = 0, -- Random value
     sleepTime0 = 0, -- Sleep timer
     looking = 0, -- facing (0 = left, 1 = right)
-    sleeping = false
+    sleeping = false -- if its sleeping or not
   }
   
   --? flush data 
@@ -101,22 +121,21 @@ local menu = {
   -- (9:wip)
   items = {
     {name = "info", action = function() 
-      print("Menu position 0 selected") 
+      debugPrint(time, debugBool,"info","Menu position 0 selected") 
     end},
     {name = "feed", action = function() 
-      print("Menu position 1 selected") 
+      debugPrint(time, debugBool,"info","Menu position 1 selected") 
     end},
     {name = "train", action = function() 
-      print("Menu position 2 selected") 
+      debugPrint(time, debugBool,"info","Menu position 2 selected") 
     end},
     {name = "challange", action = function() 
-      print("Menu position 3 selected") 
+      debugPrint(time, debugBool,"info","Menu position 3 selected") 
     end},
     {name = "flush", action = function() 
-      print("Menu position 4 selected") 
+      debugPrint(time, debugBool,"info","Menu position 4 selected") 
       -- if sleeping = true we cant flush
       if digimon.sleeping == false then
-        
         -- if room lights are of theres also no reason to flush
         if room.lights == true then
           flush.queue = 1 
@@ -125,7 +144,7 @@ local menu = {
       end
     end},
     {name = "lights", action = function() 
-      print("Menu position 5 selected")
+      debugPrint(time, debugBool,"info","Menu position 5 selected")
       -- turn on and off room lights
       if room.lights == false then
         room.lights = true 
@@ -134,16 +153,16 @@ local menu = {
       end
     end},
     {name = "patch", action = function() 
-      print("Menu position 6 selected") 
+      debugPrint(time, debugBool,"info","Menu position 6 selected") 
     end},
     {name = "Evo/info Album", action = function() 
-      print("Menu position 7 selected") 
+      debugPrint(time, debugBool,"info","Menu position 7 selected") 
     end},
     {name = "Online", action = function() 
-      print("Menu position 8 selected") 
+      debugPrint(time, debugBool,"info","Menu position 8 selected") 
     end},
     {name = "wip", action = function() 
-      print("Menu position 9 selected") 
+      debugPrint(time, debugBool,"info","Menu position 9 selected") 
     end},
   },
   
@@ -170,11 +189,11 @@ local cursor = {
 
 --? as much as i dont want to we need to keep track of poop
 poop = {
-  r = 0,
-  value = 0,
-  hasHappend = false,
-  anim = 0,
-  pos = vec2(0, 0),
+  r = 0,-- random number from 0, 10000
+  value = 0, -- time until digimon shits itself
+  hasHappend = false, -- if it has shat itself
+  anim = 0, -- number between 1, 0
+  pos = vec2(0, 0), -- shit position
   posX = 35,
   posY = 35
 }
@@ -191,22 +210,25 @@ function deltaTimeHandler()
   end
 end
 
+--* this function sends a GET request to the specified URL and stores the provided function to be called when the request completes.
 local function fetch(wifi: Wifi, url: string, resultFunc: (response: WifiWebResponseEvent) -> ())
   local handle = wifi:WebGet(url)
   handleFuncs[handle] = resultFunc
 end
 
-local _darkRoom = 5
-local _paddingX = 8.5
-local _paddingY = 16
+--? Background values
+local _darkRoom = 5 -- We change to dark room set of sprites
+local _paddingX = 8.5 -- Padding for the X
+local _paddingY = 16 -- Padding for the Y
 local _spot = {
-one = 15,
-two = 15 *2, 
-three = 15 *3,
-four = 15 *4
+one = 15, -- The first spot of the second bg sprite
+two = 15 *2, -- The second spot of the third bg sprite
+three = 15 *3, -- The third spot of the fourth bg sprite
+four = 15 *4 -- The fourth spot of the fifth bg sprite
 }
+--* This function will draw the background acordingly to the light state
 function drawbg()
-  -- checks if lights are on or off
+  --$ Checks if lights are on or off
   if room.lights then
     vid:DrawSprite(vec2(_paddingX, _paddingY), bgs ,0 ,0, color.white, color.clear)
     vid:DrawSprite(vec2(_paddingX + _spot.one , _paddingY), bgs ,1 ,0, color.white, color.clear)
@@ -232,7 +254,8 @@ function drawbg()
     vid:DrawSprite(vec2(_paddingX + _spot.two , _paddingY + _spot.one), bgs ,2 + _darkRoom ,1, color.white, color.clear)
     vid:DrawSprite(vec2(_paddingX + _spot.three , _paddingY + _spot.one), bgs ,3 + _darkRoom ,1, color.white, color.clear)
     vid:DrawSprite(vec2(_paddingX + _spot.four , _paddingY + _spot.one), bgs ,4 + _darkRoom ,1, color.white, color.clear)
-  end
+  end --$ endof room.lights check 
+
 end
 
 
@@ -241,7 +264,7 @@ end
 --* this will draw menu sprites
 function drawMenuSprites()
 
- -- Check if the room lights are off
+ --$ Check if the room lights are off
 if room.lights == false then 
   -- Check if the digimon is sleeping
   if digimon.sleeping == true then
@@ -259,39 +282,33 @@ else
       -- If the digimon is sleeping, draw (draw zz sleeping particles on a lit room)
       vid:DrawSprite(digimon.pos + vec2(15, room.r), menuSprites, 6, room.r, color.white, color.clear ) 
   end
-end
+end--$ endof room.lights check
 
-
-
-  
-
-
-
-  -- makes the menu borders
+  --! makes the menu borders $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
   vid:DrawRect(vec2(7, 15), vec2(70, 47), color.black)
   -- two lil cheeky borders on the sides so it hides the digimon 
-  vid:FillRect(vec2(0, 15), vec2(6, 47), color.white)
-  vid:FillRect(vec2(71, 15), vec2(79, 47), color.white)
+  vid:FillRect(vec2(0, 15), vec2(6, 47), Color(0,4,25))
+  vid:FillRect(vec2(71, 15), vec2(79, 47), Color(0,4,25)) 
+  --! $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-
-
-      
-  -- Draws the top menu
+  --! Draws the top menu %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   vid:DrawSprite(vec2(5,5), menuSprites, 0, 0, color.white, color.clear)
   vid:DrawSprite(vec2(20,5), menuSprites, 1, 0, color.white, color.clear)
   vid:DrawSprite(vec2(35,5), menuSprites, 2, 0, color.white, color.clear)
   vid:DrawSprite(vec2(50,5), menuSprites, 3, 0, color.white, color.clear)
   vid:DrawSprite(vec2(65,5), menuSprites, 4, 0, color.white, color.clear)
+  --!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  -- draw the bottom menu
+  --! draw the bottom menu @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   vid:DrawSprite(vec2(5,50), menuSprites, 0, 1, color.white, color.clear)
   vid:DrawSprite(vec2(20,50), menuSprites, 1, 1, color.white, color.clear)
   vid:DrawSprite(vec2(35,50), menuSprites, 2, 1, color.white, color.clear)
   vid:DrawSprite(vec2(50,50), menuSprites, 3, 1, color.white, color.clear)
   vid:DrawSprite(vec2(65,50), menuSprites, 4, 1, color.white, color.clear)
+  --!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 end
 
--- draws the cursor
+--* draws the cursor at the position
 function drawSelSprite()
     vid:DrawSprite(cursor.pos, menuSprites, 5, 1, color.white, color.clear)
 end
@@ -299,20 +316,18 @@ end
 function CursorHandler()
     -- everytime the button is clicked
     menu.current += 1 -- menu position add 1
-    cursor.posX += 15 -- move the position to 15 units
+    cursor.pos += vec2(15,0) -- move the position to 15 units
 
     -- if the cursor position is over the screen we go to next
-    if cursor.posX > 60 then
-        cursor.posY = 50
-        cursor.posX = 0
+    if cursor.pos.X > 60 then
+        cursor.pos = vec2(0, 50)
     end      
 
     -- if its over the max options we go bac
     if menu.current > menu.maxItems then
         -- reseting positions
         menu.current = 0
-        cursor.posX = 0
-        cursor.posY = 5  
+        cursor.pos = vec2(0,5)  
     end
 end
 
@@ -344,7 +359,7 @@ function digimonHandler()
   -- Clamp the value of sleepTime0 to a range of 0 to 28800 (8 hours)
   digimon.sleepTime0 = math.clamp(digimon.sleepTime0, 0, 28800)
 
-  -- Check if the digimon is currently sleeping
+  --$ Check if the digimon is currently sleeping
   if digimon.sleeping then
     -- If the digimon has been sleeping for at least 2 hours, set sleeping to false
     if digimon.sleepTime0 >= 7200 then
@@ -356,8 +371,9 @@ function digimonHandler()
     if digimon.sleepTime0 >= 28800 then
       digimon.sleepTime0 -= 28800
       digimon.sleeping = true
-    end -- #endof if digimon been awake for 8 hours
-  end -- #endof 
+    end --$ endof if digimon been awake for 8 hours
+  end --$ endof digimon.sleeping check
+
 end
 
 
@@ -366,20 +382,20 @@ end
 function poopCheck()
 
 
-    if digimon.sleeping == false then
+  if digimon.sleeping == false then
     poop.r = math.random( 0, 10000 )
 
-  
-    if poop.r == 1 or poop.value >= 10800 then
+    --$ check if conditon  poop value is 10800, check if condition poop.r is 21  
+    if poop.r == 21 or poop.value >= 10800 then
     poop.hasHappend = true
     poop.value = 0
-    end -- #endof if
-  end -- #endof sleep Check
-end -- #endof poopCheck()
+    end -- $ endof if poop condition
+  end -- $ endof sleep Check
+end 
 
 --* this function will draw poop if digimon has done the peepee poopoo caacaa
 function drawPoop()
-    if poop.hasHappend == true then
+    if poop.hasHappend then
     vid:DrawSprite(poop.pos, menuSprites, 6, poop.anim, color.white, color.clear)
     end
 end
@@ -392,7 +408,7 @@ function flushPoop()
       flush.posX += -10
     
 
-    -- if flush moved out of screen we set all values to default
+    --$ if flush moved out of screen we set all values to default
     if flush.posX < 2 then
     flush.queue = 0
     flush.posX = 55
@@ -401,11 +417,11 @@ function flushPoop()
       poop.hasHappend = false
       poop.value = 0
       end
-      -- endof poophashappend
+      --$ endof poophashappend
     end
-    --endof posX check
+    --$ endof posX check
    end
-   -- endof flushqueue
+   --$ endof flushqueue
 end
 
 --* draws the thing that flushes the shiz
@@ -453,7 +469,9 @@ end
 
 
 
-function incrementTime(timestamp)
+
+
+function incrementTime()
   time.seconds = time.seconds + 1
 
   if time.seconds >= 60 then
@@ -521,32 +539,33 @@ function spreadTimestamp(timestamp)
 end
 
 
-local function startTime()
-  debugPrint("info", "TIME IS UPDATING...")
+local function getTimeFromWeb()
+  debugPrint(time, debugBool,"info", "TIME IS UPDATING...")
+  time.health.updating = true
   -- Retrieve the current Unix timestamp from the custom API
   -- Get IP  to trow at custom api 
   fetch(web, "https://api64.ipify.org/", function(response)
     -- print ip response to see if we got the right thing
     local ip = response.Text
-    debugPrint("info", "GOT IP", ip )
+    debugPrint(time, debugBool,"info", "GOT IP", ip )
     if tonumber(ip:sub(1, 1)) then
-      time.condition = true
+      time.health.condition = true
       
       fetch(web, "http://srchforamie.com:5000/time/" .. ip, function(response)
         local time_string = response.Text
-        debugPrint("info","GOT UNIX" ,time_string)
+        debugPrint(time, debugBool,"info","GOT UNIX" ,time_string)
         if tonumber(time_string:sub(1, #time_string - 2)) then
-          debugPrint("info", "TIME HAS BEEN UPDATED")
+          debugPrint(time, debugBool,"info", "TIME HAS BEEN UPDATED")
           spreadTimestamp(time_string:sub(1, #time_string - 2))
         else
-          time.condition = false
+          time.health.condition = false
           -- There was an error with the request
-          debugPrint("error", "WEB", response.Status, response.Text)
+          debugPrint(time, debugBool,"error", "WEB", response.Status, response.Text)
         end
         
       end)
     else
-      debugPrint("error", "GOT", response.Text)
+      debugPrint(time, debugBool,"error", "GOT", response.Text)
 
     end
   end)
@@ -554,52 +573,11 @@ local function startTime()
 end
 
 
---*###
---*### prints the debug info
---*###
-function debugPrint(level, ...)
-  local levels = {
-    ["error"] = 1,
-    ["warning"] = 2,
-    ["info"] = 3,
-    ["debug"] = 4,
-    ["time"] = 5,
-  }
 
-  -- Only show debugging messages if the `debug` flag is set to `true`
-  if debugB then
-    -- Add timestamps to the output
-    local timestamp = string.format("[%02d:%02d:%02d]", time.hours, time.minutes, time.seconds)
 
-    if level == "error" then
-      setFgColor(91) -- red
-      print(timestamp, "ERROR:", ...)
-      setFgColor(39) -- default color
-    elseif level == "warning" then
-      setFgColor(93) -- yellow
-      print(timestamp, "WARNING:", ...)
-      setFgColor(39) -- default color
-    elseif level == "info" then
-      setFgColor(92) -- green
-      print(timestamp, "INFO:", ...)
-      setFgColor(39) -- default color
-    elseif level == "debug" then
-      setFgColor(94) -- blue
-      print(timestamp, "DEBUG:", ...)
-      setFgColor(39) -- default color
-    elseif level == "time" then
-      setFgColor(95) -- magenta
-      print(timestamp)
-      setFgColor(39) -- default color
-    else
-      setFgColor(91) -- red
-      print(timestamp, "UNKNOWN:", ...)
-      setFgColor(39) -- default color
-    end
-  end
-end
 
 local webtimeC = 0
+
 
 function runEvery(func, counting, ends)
   if counting == 0 then
@@ -615,16 +593,15 @@ end
 
 local timer = createTimer(
     gdt.CPU0,
-    1,
+    0.5,
     function() 
-      debugPrint("debug", "Pet", "X" .. digimon.pos.X, "Y" .. digimon.pos.Y  )
         webtimeC = runEvery(function()
-          startTime()
+          getTimeFromWeb()
         end,
           webtimeC,
-          500
+          5000 -- 82 minutes
         )
-        incrementTime(time.seconds)
+        incrementTime()
         -- add 1 to the poop value
         poop.value += 1
         -- keeps track of time
@@ -639,40 +616,42 @@ local timer = createTimer(
 )
 
 
-
-
+local debugTimer = createTimer( 
+    gdt.CPU0,
+    5,
+    function()
+      --todo| info about positions for debugging
+      debugPrint(time, debugBool,"debug", "Digimon", "X" .. digimon.pos.X, "Y" .. digimon.pos.Y  )
+      debugPrint(time, debugBool,"debug", "Cursor", "X".. cursor.pos.X, "Y".. cursor.pos.Y, "Menu:", menu.current)
+      
+      
+      
+      
+      --$ check if condition of time health is ok
+      if not time.health.condition and time.health.updating then
+        debugPrint(time, debugBool,"warning", "TimeNotStarted/NotUpdated YOU ARE OFFLINE")
+        -- Output: "[HH:MM:SS] WARNING: TimeNotStarted/NotUpdated YOU ARE OFFLINE"
+      end--$ endof timeHealth check
+    end)
 
 
 --! ################################################
 --! ######## MAIN GAME LOOP ########################
 --! ################################################
 function update()
-  --? check if time has been updated from web
-  if time.condition == false then
-    debugPrint("warning", "TimeNotStarted YOU ARE OFFLINE")
-    -- Output: "[HH:MM:SS] WARNING: timeNotStarted"
-  end
+  
   --! important updater 
   timer.update()
-  
-
-
-
-
+  debugTimer.update()
   -- clears the screen
-  vid:Clear(color(18,32,32))
+  vid:Clear(Color(18,14,32))
   -- draws the  background
   drawbg()
-  
-  -- set according possitions
-  cursor.pos = vec2(cursor.posX,cursor.posY)
-  poop.pos = vec2(poop.posX, poop.posY)
-  
+
   
   -- increase the counter by the CPU's DeltaTime
   timeDlt.counter += gdt.CPU0.DeltaTime
 
-  
   -- does colision for digimon
   digimonColision()
   
@@ -697,7 +676,7 @@ function update()
   
   -- draws the cursor
   drawSelSprite()
-  
+ 
 
   if but2.ButtonDown then
     if flush.ing == false and room.lights == true then
@@ -712,17 +691,17 @@ function update()
     menu:select()
   end
   
-  if but0.ButtonDown then
+if but0.ButtonDown then
     -- debug shityourself button
     -- poop.value += 4
-     digimon.pos += vec2(4, 0)
-    -- digimon.sleepTime0 += 28799
-    --if debugB == false then
-    --  debugB = true 
-    --elseif debugB == true then
-    --  debugB = false
+    -- digimon.pos += vec2(4, 0)
+     digimon.sleepTime0 += 28799
+    --if debugBool == false then
+    --  debugBool = true 
+    --elseif debugBool == true then
+    --  debugBool = false
     --end
-  end
+end
   
 
   
